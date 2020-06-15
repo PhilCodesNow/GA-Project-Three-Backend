75eba0bfee1da8e9d7ca166fa0c5d1a7a3abc854
Abraham Polk <abepolk@gmail.com>
2020-06-15 09:06:19 -0400

    Fixed up dates more

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -26,6 +26,8 @@ export default class Details extends Component {
 
 	render() {
         const contact = this.props.contacts.find(contact => contact._id === this.props.match.params.id);
+        const eventDate = new Date(contact.firstMeetContact.eventDate).toDateString();
+        const followUpDate = new Date(contact.followUpDate).toDateString();
         return (
             <>
                 <Layout logoff={this.props.logoff}>
@@ -47,12 +49,12 @@ export default class Details extends Component {
                             <>
                                 <li><u>First Met Contact Info</u></li>
                                 <li>{`Event Name: ${contact.firstMeetContact.eventName}`}</li>
-                                <li>{`Event Date: ${contact.firstMeetContact.eventDate}`}</li>
+                                <li>{`Event Date: ${eventDate}`}</li>
                                 <li>{`Event Info: ${contact.firstMeetContact.otherInfo}`}</li>
                             </>
                         }
                         <br />
-                        <li>{`Follow Up Date: ${contact.followUpDate}`}</li>
+                        <li>{`Follow Up Date: ${followUpDate}`}</li>
                         <li>{`Converstaion Notes: ${contact.conversationNotes}`}</li>
                     </ul>
                     <div className="buttons">
